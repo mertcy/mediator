d80b9c9faa86163b26cd9ce073709e77d48bf20b
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-04-25 19:30:08 +0300

    added MediatorMessage constants

---
+++ src/main/java/com/java/backend/mediator/MediatorMessage/MediatorMessage.java
@@ -0,0 +1,13 @@
+package com.java.backend.mediator.MediatorMessage;
+
+public class MediatorMessage {
+
+	public static final String CRUD_SUCCESS = " has been successfully ";
+	public static final String CRUD_FAILURE = " cannot be ";
+	public static final String CRUD_CREATE = " created ";
+	public static final String CRUD_READ = " read ";
+	public static final String CRUD_UPDATE = " updated ";
+	public static final String CRUD_DELETE = " deleted ";
+		
+	public static final String END_MESSAGE = "!";
+}
