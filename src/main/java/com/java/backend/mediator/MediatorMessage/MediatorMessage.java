383678002612c97768ef3f42bdcc4ec080ced9fe
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-05-01 15:29:38 +0300

    added new message types

--- src/main/java/com/java/backend/mediator/MediatorMessage/MediatorMessage.java
+++ src/main/java/com/java/backend/mediator/MediatorMessage/MediatorMessage.java
@@ -4,10 +4,20 @@ public class MediatorMessage {
 
 	public static final String CRUD_SUCCESS = " has been successfully ";
 	public static final String CRUD_FAILURE = " cannot be ";
+	
 	public static final String CRUD_CREATE = " created ";
 	public static final String CRUD_READ = " read ";
 	public static final String CRUD_UPDATE = " updated ";
 	public static final String CRUD_DELETE = " deleted ";
-		
+	
+	public static final String STATUS_ACTIVE = " active ";
+	public static final String STATUS_INACTIVE = " inactive ";
+	public static final String STATUS_NOTAVAILABLE = " not available ";
+	
+	public static final String SO_MESSAGE = ", so ";
 	public static final String END_MESSAGE = "!";
+	
+	public static final String ERROR_ISBLANK = "Error processing blank token!";
+	public static final String ERROR_ISEMPTY = "Error processing empty token!";
+
 }
