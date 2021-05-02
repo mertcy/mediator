0771f064d947cc166739f7a5a4bb48bf8dd51cc9
yg <yg@DESKTOP-RQHJF5E>
2021-05-02 12:04:57 +0300

    css-image

--- src/main/java/com/java/backend/mediator/MediatorApplication.java
+++ src/main/java/com/java/backend/mediator/MediatorApplication.java
@@ -1,7 +1,14 @@
 package com.java.backend.mediator;
 
+import java.util.Arrays;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MediatorApplication {
@@ -9,5 +16,18 @@ public class MediatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MediatorApplication.class, args);
 	}
+	
+	@Bean
+	  ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
+	      //spring boot only works if this is set
+	      servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+	      //registration
+	      ServletRegistrationBean srb = new ServletRegistrationBean();
+	      srb.setServlet(new FacesServlet());
+	      srb.setUrlMappings(Arrays.asList("*.xhtml"));
+	      srb.setLoadOnStartup(1);
+	      return srb;
+	  }
 
 }
