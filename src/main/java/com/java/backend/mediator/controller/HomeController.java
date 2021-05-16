fcbdafff3702425eba2faff6fdc389f445762da5
yg <yg@DESKTOP-RQHJF5E>
2021-05-16 17:11:24 +0300

    homeController

--- src/main/java/com/java/backend/mediator/controller/HomeController.java
+++ src/main/java/com/java/backend/mediator/controller/HomeController.java
@@ -4,16 +4,11 @@ package com.java.backend.mediator.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.java.backend.mediator.ButtonManagedBean;
-import com.java.backend.mediator.SpringContext;
-
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-    	//ButtonManagedBean buttonManagedBean = SpringContext.getBean(ButtonManagedBean.class);
     	return "login.xhtml";
-        //return "Hello from Mediator App Service (in the staging slot)!";
     }
 }
