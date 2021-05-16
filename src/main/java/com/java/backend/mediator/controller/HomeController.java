a52544aac780693affb2b7b08e54f7af89dcd3e2
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-05-16 11:02:32 +0300

    changed home controller for welcome page to open as sign up

--- src/main/java/com/java/backend/mediator/controller/HomeController.java
+++ src/main/java/com/java/backend/mediator/controller/HomeController.java
@@ -1,14 +1,19 @@
 
 package com.java.backend.mediator.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.java.backend.mediator.ButtonManagedBean;
+import com.java.backend.mediator.SpringContext;
+
+@Controller
 public class HomeController {
-    
+
     @GetMapping("/")
     public String home() {
-        return "Hello from Mediator App Service (in the staging slot)!";
+    	ButtonManagedBean buttonManagedBean = SpringContext.getBean(ButtonManagedBean.class);
+    	return buttonManagedBean.toSignUp();
+        //return "Hello from Mediator App Service (in the staging slot)!";
     }
 }
