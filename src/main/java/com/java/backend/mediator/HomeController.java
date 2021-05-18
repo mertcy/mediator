a2a74467977baec90228f8d10016cbc30ec31046
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-05-18 21:38:05 +0300

    moved HomeController to main package

--- src/main/java/com/java/backend/mediator/HomeController.java
+++ src/main/java/com/java/backend/mediator/HomeController.java
@@ -1,5 +1,5 @@
 
-package com.java.backend.mediator.controller;
+package com.java.backend.mediator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
