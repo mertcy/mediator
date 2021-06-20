da372e56531cfc641979136ae61d13e51a257abd
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-06-20 16:22:59 +0300

    added rateProvider service for ProviderService

--- src/main/java/com/java/backend/mediator/Provider/ProviderService.java
+++ src/main/java/com/java/backend/mediator/Provider/ProviderService.java
@@ -1,9 +1,13 @@
 package com.java.backend.mediator.Provider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.java.backend.mediator.Model.Model.Status;
+import com.java.backend.mediator.Rate.Rate;
 
 @Service
 public class ProviderService {
@@ -25,6 +29,30 @@ public class ProviderService {
     
     public List<Provider> findProviders(List<String> ids){
     	return providerRepository.findByIdIn(ids);
+    }
+    
+    public Provider rateProvider(String providerId, String consumerId, Integer rating) {  
+    	   	
+    	Rate rate = new Rate(providerId, consumerId, rating);
+    	
+    	Provider provider = this.findProviderByUserId(providerId);
+
+    	ArrayList<Rate> ratings = provider.getRatings();
+        if(ratings == null) {
+        	ratings = new ArrayList<Rate>();
+        } else {
+        	for(Rate r : ratings) {        		
+        		if(r.getConsumerId().equals(consumerId)) {
+        			if(r.getStatus().equals(Status.ACTIVE)) {        				
+            			r.setStatus(Status.INACTIVE);        				
+        			}
+        		}      		
+        	}
+        }        
+		ratings.add(rate);       				
+    	provider.setRatings(ratings);
+
+    	return this.saveProvider(provider);
     }
 
 }
