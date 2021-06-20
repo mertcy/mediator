693697a60501ca5597845e8c42784a2c16dcba67
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2021-06-20 16:23:44 +0300

    added ratings list for Provider entity

--- src/main/java/com/java/backend/mediator/Provider/Provider.java
+++ src/main/java/com/java/backend/mediator/Provider/Provider.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.data.annotation.Transient;
 
 import com.java.backend.mediator.Model.Model;
+import com.java.backend.mediator.Rate.Rate;
 import com.java.backend.mediator.ServiceProvided.ServiceProvided;
 
 public class Provider extends Model {
@@ -14,11 +15,14 @@ public class Provider extends Model {
 	
 	public ArrayList<ServiceProvided> servicesProvided;
 
+	public ArrayList<Rate> ratings;
+	
 	public Provider() {}
 	
 	public Provider(String userId) {
 		super.setId(userId);
 		setServicesProvided(new ArrayList<ServiceProvided>());
+		setRatings(new ArrayList<Rate>());
 	}
 	
 	public ArrayList<ServiceProvided> getServicesProvided() {
@@ -27,6 +31,14 @@ public class Provider extends Model {
 
 	public void setServicesProvided(ArrayList<ServiceProvided> servicesProvided) {
 		this.servicesProvided = servicesProvided;
+	}
+
+	public ArrayList<Rate> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(ArrayList<Rate> ratings) {
+		this.ratings = ratings;
 	}
 	
 }
